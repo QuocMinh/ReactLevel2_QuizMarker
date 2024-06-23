aaa2d2dfb5a8dc2e91e1eb9dc9d6efc5c3128828
QuocMinh <cqminh1995@gmail.com>
2024-06-23 23:13:37 +0700

    Update for deploy Github

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,15 @@ import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/ReactLevel2_QuizMarker",
   plugins: [react()],
   resolve: {
     alias: [
       { find: "@api", replacement: resolve(__dirname, "src/api") },
-      { find: "@components", replacement: resolve(__dirname, "src/components"), },
+      {
+        find: "@components",
+        replacement: resolve(__dirname, "src/components"),
+      },
       { find: "@constants", replacement: resolve(__dirname, "src/constants") },
       { find: "@models", replacement: resolve(__dirname, "src/models") },
       { find: "@partials", replacement: resolve(__dirname, "src/partials") },
